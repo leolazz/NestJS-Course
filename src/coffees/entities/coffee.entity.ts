c6909e0d5e5aaca6dfa4a4f2bfe8ba9ad4bde26f
Leo <leolazzarini101@gmail.com>
2021-07-22 16:42:56 -0700

    compose filed updated with pgAdmin container

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+// sql table === 'coffee'. 
+// to change the table enter it in qoutes in the parenthesis of the entity decerator
+@Entity()
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+    
+    @Column('json', {nullable: true})
     flavors: string[];
 }
