7e85815f3a8fdf9f9769f309e42cdf6698967dbe
Leo <leolazzarini101@gmail.com>
2021-07-26 15:05:51 -0700

    app config section. added .env for app & docker

---
+++ src/coffee-rating/coffee-rating.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
+import { CoffeeRatingService } from './coffee-rating.service';
+
+@Module({
+  imports: [
+    DatabaseModule.register({ // 👈 passing in dynamic values
+      type: 'postgres',
+      host: 'localhost',
+      password: 'password',
+    })
+  ],
+  providers: [CoffeeRatingService],
+})
+export class CoffeeRatingModule {}
